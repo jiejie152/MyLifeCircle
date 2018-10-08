94617bd236b159f5e8ab192b26f7592e971698b5
unknown <unknown@example.com>
2018-10-08 23:10:57 +0800

    Initial Commit

---
+++ pages/context/app1/app1.js
@@ -0,0 +1,18 @@
+Page({
+  data: {
+    texts: [1, 2],
+    iconType: [
+      'search', 'clear'
+    ],
+    services: [{
+      url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1571987018,3603529578&fm=26&gp=0.jpg',
+        name: '1'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3761686212,624295118&fm=26&gp=0.jpg',
+        name: '2'
+      }
+    ]
+  },
+  onLoad: function() {}
+})
