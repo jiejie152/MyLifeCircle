b94373f218907b5da90e901c7872786bec472eca
unknown <unknown@example.com>
2018-10-09 10:22:30 +0800

    修改图标显示效果

--- pages/context/app1/app1.js
+++ pages/context/app1/app1.js
@@ -11,6 +11,38 @@ Page({
       {
         url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3761686212,624295118&fm=26&gp=0.jpg',
         name: '2'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3761686212,624295118&fm=26&gp=0.jpg',
+        name: '2'
+      }, 
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3761686212,624295118&fm=26&gp=0.jpg',
+        name: '2'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3761686212,624295118&fm=26&gp=0.jpg',
+        name: '2'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1571987018,3603529578&fm=26&gp=0.jpg',
+        name: '1'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1571987018,3603529578&fm=26&gp=0.jpg',
+        name: '1'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1571987018,3603529578&fm=26&gp=0.jpg',
+        name: '1'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1571987018,3603529578&fm=26&gp=0.jpg',
+        name: '1'
+      },
+      {
+        url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1571987018,3603529578&fm=26&gp=0.jpg',
+        name: '1'
       }
     ]
   },
